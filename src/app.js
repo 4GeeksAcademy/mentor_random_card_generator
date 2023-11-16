c3534c10c63243a19e94c39adcb333e6aada04e0
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2023-11-16 23:46:32 +0000

    the card could look better

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
-/* eslint-disable */
-import "bootstrap";
+// /* eslint-disable */
+// import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   generateRandomCard();
+// };
+
+// function generateRandomCard() {
+//   let top = document.querySelector(".top");
+//   let number = document.querySelector(".number");
+//   let bottom = document.querySelector(".bottom");
+
+//   let allSuits = ["♦", "♣", "♥", "♠"];
+//   let allNumbers = [
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "K",
+//     "Q",
+//     "J",
+//     "A"
+//   ];
+
+//   randomSuit = allSuits[Math.floor(Math.random() * allSuits.length)];
+//   randomNumbers = allNumbers[Math.floor(Math.random() * allNumbers.length)];
+
+//   if (randomSuit == "♥" || randomSuit == "♦") {
+//     top.style.color = "red";
+//     number.style.color = "red";
+//     bottom.style.color = "red";
+//   } else {
+//     top.style.color = "black";
+//     number.style.color = "black";
+//     bottom.style.color = "black";
+//   }
+
+//   top.innerHTML = randomSuit;
+//   number.innerHTML = randomNumber;
+//   bottom.innerHTML = randomSuit;
+// }
